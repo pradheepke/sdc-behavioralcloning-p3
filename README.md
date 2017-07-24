fbe788ffd7f043f51aa8b84d9f24de5a5dc52699
pradheep <pradheep.ke@gmail.com>
2017-07-23 20:18:19 -0700

    Committing project #3

---
+++ README.md
@@ -0,0 +1,29 @@
+# Behaviorial Cloning Project
+
+## Model architecture
+
+Tried simple first to make sure end to end is working.
+
+nVidia architecture.
+
+Overfitting/underfitting detection.
+
+## Sequence of steps to improve:
+ - Flip LR.
+ - 1000 images. 
+ - Checkpointing for convenient early stopping
+
+ - Simulation
+   - Went opposite and drove clockwise. Just about 2K images.
+
+ - Interesting to see what happens without the augmented data.
+  - Note validation vs. training loss changes. 
+  - Lesser overfitting in the new one. Still there is.
+
+ - Wanted to see how much we could do just with CPU.
+
+ - How does one robustly identify why something is failing? 
+Other than blackbox system level heuristics? 
+How to decide what kind of data that I should augment it with?
+
+
---
+++ model.py
@@ -0,0 +1,97 @@
+import csv
+import cv2
+import numpy as np
+import sklearn
+from keras.callbacks import ModelCheckpoint
+
+
+data_root_path = 'data/'
+driving_log_file = 'data/driving_log.csv'
+driving_log_from_sim_file = 'data_from_sim/trial1/driving_log.csv'
+model_output_root = 'out/'
+lines = []
+
+def generator(samples, batch_size=16):
+    num_samples = len(samples)
+    while 1:
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            images = []
+            angles = []
+
+            for batch_sample in batch_samples:
+                imgpath = data_root_path + batch_sample[0]
+                if batch_sample[0][0] == '/':
+                    imgpath = batch_sample[0]
+
+                img = cv2.imread(imgpath)
+                angle = float(batch_sample[3])
+                images.append(img)
+                angles.append(angle)
+                
+                img_flipped = np.fliplr(img)
+                angle_flipped = -angle
+                images.append(img_flipped)
+                angles.append(angle_flipped)
+            
+            X_train_batch = np.array(images)
+            y_train_batch = np.array(angles)
+
+            yield sklearn.utils.shuffle(X_train_batch, y_train_batch)
+                    
+
+def load_driving_log(driving_log_file):
+    lines = []
+    i = 0
+    with open(driving_log_file) as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        for line in reader:
+            i = i + 1
+            lines.append(line)
+            #  For dev/debug
+            #        if i >= 100:
+            #            break
+    return lines
+    
+lines = load_driving_log(driving_log_file)
+lines.extend(load_driving_log(driving_log_from_sim_file))
+
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(
+   lines, test_size=0.2)
+
+train_generator = generator(train_samples)
+validation_generator = generator(validation_samples)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0,0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam', metrics=['mse'])
+
+filepath="out/model-impr-{epoch:02d}-{val_mean_squared_error:.4f}.h5"
+checkpoint = ModelCheckpoint(filepath, 
+                             monitor='val_mean_squared_error', 
+                             save_best_only=True, mode='min')
+model.fit_generator(train_generator, 
+                    samples_per_epoch=len(train_samples) * 2,
+                    validation_data=validation_generator,
+                    nb_val_samples=len(validation_samples) * 2,
+                    nb_epoch=10,
+                    callbacks=[checkpoint])
+
+model.save('out/model.h5')
